07ce6164591afb8312776b79ae06da139ba7557d
Wietse Franssen <wietse.franssen@wur.nl>
2018-01-03 16:02:32 +0100

    Project setup complete

---
+++ R/vic4to5.R
@@ -0,0 +1,13 @@
+#' VIC4to5
+#'
+#' @description Convert VIC version 4.x ascii input data to VIC-WSG (VIC version 5.x) NetCDF input data
+#' @author Wietse Franssen \email{wietse.franssen@@wur.nl}
+#' @param soilfile the VIC-4 soilfile
+#' @return NetCDF input file for VIC-WSG
+#' @import ncdf4
+#' @export
+#' @examples
+#' vic4to5()
+vic4to5 <- function(soilfile) {
+  print("Hello, world!")
+}
